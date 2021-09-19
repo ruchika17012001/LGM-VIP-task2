9b10a7dd797747450d0776116da78e34d53bf56e
ruchika17012001 <86402725+ruchika17012001@users.noreply.github.com>
2021-09-19 00:42:26 -0700

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return (
+            <>
+            <div className="text-center new bord">
+             <p className= "let">Lets Grow More</p>   
+            <h2>Copyright &copy; ReactApp</h2>
+            </div>
+             </>
+    )
+}
